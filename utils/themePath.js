4aa233a7b821a9ab782ba6d1206122add0610c39
aleewr <121657771@qq.com>
2018-05-20 21:47:30 +0800

    theme copy

--- utils/themePath.js
+++ utils/themePath.js
@@ -3,12 +3,14 @@ const path = require('path')
 
 function themePath (name) {
     // 加载node_module中的
+    console.log('name: ' + name)
     let themeSrc = ''
     try {
-        const modulePath = path.resolve(process.cwd(), 'node_modules', 'aldoc-theme-' + name)
+        const modulePath = path.resolve(process.cwd(), 'node_modules', name)
         themeSrc = require(modulePath)
     } catch (e) {
-        const modulePath = path.resolve(process.cwd(), '../node_modules', 'aldoc-theme-' + name)
+        const themeSrc = path.resolve(__dirname, '../node_modules', name)
     }
+    return themeSrc
 }
 module.exports = themePath
